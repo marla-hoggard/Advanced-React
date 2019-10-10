1fec70b15dbc2e0b4eb534e93d60ba33a2d1195a
Marla Glasser <mbglasser@gmail.com>
2019-10-10 14:55:14 -0400

    Set up Prisma and GraphQL Yoga. Add item type, items query and createItem mutation.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const { forwardTo } = require('prisma-binding');
 
-module.exports = mutations;
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+
+    return item;
+  }
+};
+
+module.exports = Mutations;
