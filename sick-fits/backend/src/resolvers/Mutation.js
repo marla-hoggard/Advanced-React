808a8f123d7e8722861d4930b18210b384aca139
Marla Glasser <mbglasser@gmail.com>
2019-10-17 15:48:42 -0400

    Update User datamodel. Add signup mutation. Pull in cookies and JWT.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,4 +1,5 @@
-const { forwardTo } = require('prisma-binding');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -32,6 +33,38 @@ const Mutations = {
     // TODO
     // 3. Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    // Hash their password
+    // Pass in PW and SALT length for uniqueness between sites
+    const password = await bcrypt.hash(args.password, 10);
+
+    // Create the user in the db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER']},
+        }
+      }, 
+      info,
+    );
+
+    // Create JWT token for user
+    const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
+
+    // Set JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie (in ms)
+    });
+
+    // Return user to the browser
+    return user;
   },
 };
 
