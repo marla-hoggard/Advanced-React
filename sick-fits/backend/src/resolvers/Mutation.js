d79a7d217bf52f3b34c939e1237f4a48c8916c4f
Marla Glasser <mbglasser@gmail.com>
2019-10-21 15:47:45 -0400

    Sign in and Sign out flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -66,6 +66,39 @@ const Mutations = {
     // Return user to the browser
     return user;
   },
+
+  async signin(parent, args, ctx, info) {
+    const { email, password } = args;
+
+    // 1. Check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email }});
+    if (!user) {
+      throw new Error(`No such user found for email: ${email}`);
+    }
+
+    // 2. Check if their password is correct
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      throw new Error(`Invalid Password!`);
+    }
+
+    // 3. Generate JWT token
+    const token = jwt.sign({ userId: user.id}, process.env.APP_SECRET);
+
+    // 4. Set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie (in ms)
+    });
+
+    // 5. Return the user
+    return user;
+  },
+
+  async signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'You have been successfully signed out!' };
+  },
 };
 
 module.exports = Mutations;
