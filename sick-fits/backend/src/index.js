aff9642450bb399432d2e352550151fee457da58
Marla Glasser <mbglasser@gmail.com>
2019-10-21 14:42:44 -0400

    Add user id from JWT to cookies. Write me query for current user.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -2,13 +2,22 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const server = createServer();
 
 // Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 
-// TODO: Use express middleware to populate current user
+// Decode JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+})
 
 server.start(
   {
