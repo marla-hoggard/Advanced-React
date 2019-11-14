f9be9cb73d9263d3c13262cdeb1914e61b7e4fa0
Marla Glasser <mbglasser@gmail.com>
2019-11-14 00:19:12 -0500

    Address deprecated features from next upgrade

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import { ApolloProvider } from 'react-apollo';
 
 import Page from '../components/Page';
@@ -7,9 +7,9 @@ import withData from '../lib/withData';
 class MyApp extends App {
   // This is a special Next.js lifecycle
   // This is going to fetch all the queries and mutations from the page
-  static async getInitialProps({ Component, ctx}) {
+  static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
-    if(Component.getInitialProps) {
+    if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
     // This exposes the query to the user
@@ -20,13 +20,11 @@ class MyApp extends App {
     const { Component, apollo, pageProps } = this.props;
 
     return (
-      <Container>
-        <ApolloProvider client={apollo}>
-          <Page>
-            <Component {...pageProps} />
-          </Page>
-        </ApolloProvider>
-      </Container>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
     )
   }
 }
